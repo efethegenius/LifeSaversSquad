f67d17d56ca722065b1d750262561565952362b7
Samuel Efe Fortune <samuelefe.37@gmail.com>
2022-06-23 21:40:27 +0100

    yu

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
-  next();
+  if ("OPTIONS" == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 //Login-------------------------------------------------------------------------------------------------------------------------------
